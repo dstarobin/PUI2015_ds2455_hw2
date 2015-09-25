86281a3b10e2018862e7756883407189c4a0ae64
dstarobin <ds2455@nyu.edu>
2015-09-24 20:12:22 -0400

    Updated to conform more closely with PEP8

--- show_bus_locations.py
+++ show_bus_locations.py
@@ -1,6 +1,3 @@
-#This script retrieves/ reports info about locations of all active vehicles on one MTA bus line
-#my API key = 4280fc9e-4ec8-4106-ba00-6c4cc71597d3
-#full url = 'http://api.prod.obanyc.com/api/siri/vehicle-monitoring.json?key=4280fc9e-4ec8-4106-ba00-6c4cc71597d3&VehicleMonitoringDetailLevel=calls&LineRef=B63'
 #to use in command line: python show_bus_locations.py <BusTime API key> <Bus route #, e.g. B52>
 
 import json
@@ -10,25 +7,28 @@ import urllib2
 API_key = sys.argv[1]
 busroute = sys.argv[2]
 
-if __name__=='__main__':
-  #accepts command line args for API key and bus line name (e.g. B61 etc.)
-	url = 'http://api.prod.obanyc.com/api/siri/vehicle-monitoring.json?key={}&VehicleMonitoringDetailLevel=calls&LineRef={}'.format (API_key, busroute)
-	request = urllib2.urlopen(url)
-	busdata = json.load(request)
+if __name__ == '__main__':
+    # accepts command line args for API key and bus line name (e.g. B61 etc.)
+    url = 'http://api.prod.obanyc.com/api/siri/vehicle-monitoring.json?key={}&VehicleMonitoringDetailLevel=calls&LineRef={}'.format(
+        API_key, busroute)
+    request = urllib2.urlopen(url)
+    busdata = json.load(request)
 
-	activity = busdata['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']
+    activity = busdata['Siri']['ServiceDelivery'][
+        'VehicleMonitoringDelivery'][0]['VehicleActivity']
 
-	num_buses = 0
-	for i in activity:
-		num_buses += 1
+    num_buses = 0
+    for i in activity:
+        num_buses += 1
 
-	print "Bus Line: %s" % (sys.argv[2])	
-	print "Number of Active Buses:", num_buses
+    print "Bus Line: %s" % (sys.argv[2])
+    print "Number of Active Buses:", num_buses
 
-	print_count = 0
-		
-	for i in activity:
-		print_count += 1
-		latitude = i['MonitoredVehicleJourney']['VehicleLocation']['Latitude'] 
-		longitude = i['MonitoredVehicleJourney']['VehicleLocation']['Longitude']
-		print "Bus %s is at latitude %s, longitude %s." % (print_count, latitude, longitude)
+    print_count = 0
+
+    for i in activity:
+        print_count += 1
+        latitude = i['MonitoredVehicleJourney']['VehicleLocation']['Latitude']
+        longitude = i['MonitoredVehicleJourney'][
+            'VehicleLocation']['Longitude']
+        print "Bus %s is at latitude %s, longitude %s." % (print_count, latitude, longitude)
